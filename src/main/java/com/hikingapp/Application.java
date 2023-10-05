99526728a3acb32c7f4d5ccb1bb0fec2df6acfb9
Mattia Sorzè <mattia.sorze@mail.polimi.it>
2023-10-06 00:53:49 +0200

    aggiunto cors filter

--- src/main/java/com/hikingapp/Application.java
+++ src/main/java/com/hikingapp/Application.java
@@ -1,15 +1,21 @@
 package com.hikingapp;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.hikingapp.persistence.repository.HikingAppBaseRepositoryImpl;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @EnableJpaRepositories (repositoryBaseClass = HikingAppBaseRepositoryImpl.class)
@@ -32,6 +38,20 @@ public class Application {
 			}
 
 		};
+	}
+
+	@Bean
+	public FilterRegistrationBean corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOriginPattern("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+		bean.setOrder(0);
+		return bean;
 	}
 
 }
